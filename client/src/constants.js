a1b9e0884939cf55a25f283dcaa5b844c963f891
shashankbalabhadra <shashank.balabhadra@gmail.com>
2020-09-09 11:30:26 +0530

    remove client folder

--- client/src/constants.js
+++
@@ -1,7 +0,0 @@
-export const USERNAME_LABEL = "Username";
-export const USERNAME_PLACEHOLDER = "Enter username";
-export const PASSWORD_LABEL = "Password";
-export const PASSWORD_PLACEHOLDER = "Enter password";
-export const SIGN_IN = "Sign in";
-export const INVALID_CREDENTIALS = "invalid credentials";
-export const NETWORK_ERROR = "network error";
--- client/src/utils/theme/theme.js
+++
@@ -1,8 +0,0 @@
-import { createMuiTheme } from "@material-ui/core/styles";
-
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: "Poppins",
-  },
-});
-export default theme;
